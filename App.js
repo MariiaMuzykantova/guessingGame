d6fc4f742e6e29a86e306c3a3d1510a184aa7c37
MariiaMuzykantova <32201576+MariiaMuzykantova@users.noreply.github.com>
2018-02-12 12:54:19 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {num: '', text: ''};
-    this.state = {answer: Math.floor(Math.random() * 100) + 1}
+    this.state = {
+      num: '',
+      message: 'Guess a number between 1-100',
+      correctanswer: Math.floor(Math.random() * 100) + 1,
+      counter: 0
+    }
   }
 
-  buttonPressed = () => {
-    const rendom = 
-    this.setState({
-      result: sum
-    });
+  EvaluateAnswer = () => {
+    this.setState({ counter: this.state.counter +1});
+   if (this.state.num == this.state.correctanswer) {
+      Alert.alert('Your guessed the number in ' + this.state.counter + ' guesses');
+    }
+    else if (this.state.num < this.state.correctanswer) {
+      this.setState({ message: 'Your guess ' + this.state.num + ' is too low'});
+    }
+    else {
+        this.setState({ message: 'Your guess ' + this.state.num + ' is too high'});
+    }
   }
-
-  
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Guess a number between 1-100</Text>
-        <TextInput value = {this.state.num} 
+        <Text>{this.state.message}</Text>
+        <TextInput value = {this.state.num}
         style = {{width: 100, borderColor: 'gray', borderWidth: 1}}
         keyboardType = 'phone-pad'
-        onChangeText = {(num) => this.setState({num})}/>
-
-        <Button onPress = {this.buttonPressed} title = "MAKE GUESS" style = {styles.button}  color = '#0066ff'/>
+        onChangeText = {(num) => this.setState({num})}
+        />
+        <Button onPress = {this.EvaluateAnswer} title = "MAKE GUESS" style = {styles.button}  color = '#0066ff'/>
       </View>
     );
   }
